1206f3cde6eca87c53558fce055fc2c13db78c49
Elafifi Nour-Eldin <59283990+Nour-ElMasry@users.noreply.github.com>
2022-07-22 21:19:08 +0300

    First commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,108 @@
+﻿namespace ClassLibrary1;
+public class College
+{
+    List<Prof> Profs;
+    List<Stud> Studs;
+    public string Name { get; set; }
+
+    public College(string name)
+    {
+        Name = name;
+        Profs = new List<Prof>();
+        Studs = new List<Stud>();
+    }
+
+    public override string ToString()
+    {
+        return "Name: " + Name + PrintLists();
+    }
+
+    public string PrintLists()
+    {
+        string profString = "";
+        string studString = "";
+
+        Profs.ForEach(p => profString += p.ToString());
+        Studs.ForEach(s => studString += s.ToString());
+
+        return "\nProfs: [" + profString + "]\nStuds: [" + studString + "]";
+    }
+
+    public void AddProf(Prof p)
+    {
+        p.ResidingCollege = Name;
+        Profs.Add(p);
+    }
+
+    public void AddStud(Stud s)
+    {
+        s.ResidingCollege = Name;
+        Studs.Add(s);
+    }
+}
+
+public abstract class Person : IPerson
+{
+    public string Name;
+    public int Age;
+
+    public abstract void About();
+    public void Intro()
+    {
+        Console.WriteLine("Hi, my name is " + Name + "! I am " + Age + " years old.");
+    }
+}
+
+public interface IPerson
+{
+    void Intro();
+}
+
+public class Prof : Person
+{
+    public string TeachingSubject { get; set; }
+    public string ResidingCollege = "";
+
+    public Prof(string name, int age, string teachingSubject)
+    {
+        Name = name;
+        Age = age;
+        TeachingSubject = teachingSubject;
+    }
+
+    public override string ToString()
+    {
+        return "{ Name: " + Name + ", Age: " + Age + ", TeachingSubject: " + TeachingSubject + " }, ";
+    }
+
+    public override void About()
+    {
+        Intro();
+        Console.WriteLine("I am a professor at " + ResidingCollege);
+    }
+}
+
+public class Stud : Person
+{
+    public string Group { get; set; }
+    public string ResidingCollege = "";
+
+    public Stud(string name, int age, string group)
+    {
+        Name = name;
+        Age = age;
+        Group = group;
+    }
+
+    public override string ToString()
+    {
+        return "{ Name: " + Name + ", Age: " + Age + ", Group: " + Group + " }, ";
+    }
+
+    public override void About()
+    {
+        Intro();
+        Console.WriteLine("I am a student at " + ResidingCollege);
+    }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ClassLibrary1;
+
+
+namespace ConsoleApp 
+{ 
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            var politehnica = new College("Politehnica");
+
+            var vasi = new Prof("Vasi", 45, "OOP");
+
+            var nour = new Stud("Nour", 21, "1231A");
+
+            var laur = new Stud("Laur", 22, "1231A");
+
+            politehnica.AddProf(vasi);
+            politehnica.AddStud(nour);
+            politehnica.AddStud(laur);
+
+
+            Console.WriteLine("College info {\n" + politehnica.ToString() + "\n}\n\n");
+
+            Console.WriteLine("About Nour { \n");
+            nour.About();
+
+            Console.WriteLine("\n}\n\nAbout Laur { \n");
+            laur.About();
+
+            Console.WriteLine("\n}\n\nAbout Vasi { \n");
+            vasi.About();
+            Console.WriteLine("\n}");
+
+        }
+    }
+}
+
+
